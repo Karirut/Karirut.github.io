d69b5d85eed9e91e55273bee3d65696882c074c3
Karina Ruiz Tron <89891435+Karirut@users.noreply.github.com>
2024-05-09 16:46:03 -0600

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -114,14 +114,14 @@ const CONFIG = {
       companyLink: 'https://example.com',
     },
   ],
-//  certifications: [
+  certifications: [
 //    {
 //      name: 'Lorem ipsum',
 //      body: 'Lorem ipsum dolor sit amet',
 //      year: 'March 2022',
 //      link: 'https://example.com',
 //    },
-//  ],
+  ],
   educations: [
     {
       institution: 'Institution Name',
@@ -130,7 +130,7 @@ const CONFIG = {
       to: '2019',
     },
   ],
-//  publications: [
+  publications: [
 //    {
 //      title: 'Publication Title',
 //      conferenceName: '',
@@ -149,7 +149,7 @@ const CONFIG = {
 //      description:
 //        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 //    },
-//  ],
+  ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
 //    source: 'dev', // medium | dev
@@ -229,7 +229,7 @@ const CONFIG = {
 
   // Optional Footer. Supports plain text or HTML.
   footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
+      class="text-primary" href="https://github.com/Karirut/gitprofile"
       target="_blank"
       rel="noreferrer"
     >GitProfile</a> and ❤️`,
